3a741f5bafcdcd8ee7e85d4febeea6cea36d54e8
Nikita Kiselov <kinivi210999@gmail.com>
2018-12-16 23:07:29 +0200

    Fixing starting position

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
 'use strict'
 
+let povtorkaIsAnimating = false;
 let povtorka = {
     targets: '#povtorka',
     rotate: {
         value: 135,
         duration: 0
     },
-    translateX: -250,
-    translateY: -250,
+    translateX: -350,
+    translateY: -350,
     direction: 'alternate',
     duration: 300,
-    scale: 2
+    scale: 1.5,
+    oncomplete: () => {
+        povtorkaIsAnimating = false;
+}
 };
 
-let animeJS = anime(povtorka);
+let animeJS;
 
-window.onkeypress = function(){animeJS.restart()};
+window.onkeypress = function(){
+    if(povtorkaIsAnimating) {
+        animeJS.restart();
+    } else {
+        animeJS = anime(povtorka);
+        povtorkaIsAnimating = true;
+    }
+
+};
 
 
 
