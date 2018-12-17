532502a6e2570d2be03d52a7659f92675fab8f5f
Nikita Kiselov <kinivi210999@gmail.com>
2018-12-17 18:47:40 +0200

    Auto stash before merge of "master" and "origin/master"

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 'use strict'
 
 let povtorkaIsAnimating = false;
+let animes = [];
+let animesConfigs = [];
+let sounds = [];
+
+let icqSound = new Howl({
+    src: ['sounds/icq_old_sound.mp3']
+});
+
+let screamSound = new Howl({
+    src: ['sounds/scream.mp3']
+});
+
+sounds[13] = icqSound;
+sounds[17] = screamSound;
+
+let povtorkaAnime;
+let screamAnime;
+
 let povtorka = {
     targets: '#povtorka',
     rotate: {
@@ -12,22 +30,50 @@ let povtorka = {
     direction: 'alternate',
     duration: 300,
     scale: 1.5,
-    oncomplete: () => {
-        povtorkaIsAnimating = false;
+    isAnimating: false,
+    code: 13
+};
+
+let scream = {
+    targets: '#scream',
+    rotate: '3turn',
+    translateX: -350,
+    translateY: -350,
+    direction: 'alternate',
+    duration: 3000,
+    scale: 3,
+    isAnimating: false,
+    code: 17
+};
+
+
+animesConfigs[13] = povtorka;
+animesConfigs[17] = scream;
+
+function choosingAnime(keycode) {
+    animate(animesConfigs[keycode], sounds[keycode]);
 }
-};
 
-let animeJS;
+function animate(animationProperty, sound) {
+    if (animationProperty.isAnimating) {
+        animes[animationProperty.code].restart();
+        sound.play();
 
-window.onkeypress = function(){
-    if(povtorkaIsAnimating) {
-        animeJS.restart();
     } else {
-        animeJS = anime(povtorka);
-        povtorkaIsAnimating = true;
+        animes[animationProperty.code] = anime(povtorka);
+        animationProperty.isAnimating = true;
+        sound.play();
     }
+}
 
-};
+function checkKeyPressed(evt) {
+    choosingAnime(parseInt(evt.keyCode));
+}
+
+window.addEventListener("keydown", checkKeyPressed, false);
+
+anime(scream);
+
 
 
 
